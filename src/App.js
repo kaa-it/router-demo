05b4750c805afb6850af7f7b22e0d2387ecffa62
Andrey Kruglov <kruglov.it@gmail.com>
2022-11-23 22:22:06 +0300

    custom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Settings } from "./pages/Settings";
+import React, {useContext, useEffect, useState} from "react";
+import {ownHistory} from "./history";
+
+const RouterContext = React.createContext(window.location.pathname);
+
+function Router({ children }) {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        return ownHistory.listen(location => setCurrentPath(location));
+    }, []);
+
+    return <RouterContext.Provider value={ currentPath }>
+        {children}
+    </RouterContext.Provider>;
+}
+
+function Route({ path, children }) {
+    const currentPath  = useContext(RouterContext);
+    return currentPath === path && children;
+}
+
+function Link({href, children}) {
+    function handleClick(e) {
+        e.preventDefault();
+        ownHistory.pushState(href);
+    }
+    return <a href={href} onClick={handleClick}>{children}</a>
+}
 
 const App = () => {
+  const [url, setUrl] = useState('');
+
   return (
     <>
-      <Home />
-      <Login />
-      <Settings />
+        <Link href="/home">Home</Link>
+        <Link href="/login">Login</Link>
+        <Link href="/settings">Settings</Link>
+
+      <Router currentPath={url}>
+          <Route path='/home'>
+              <Home />
+          </Route>
+          <Route path='/login'>
+              <Login />
+          </Route>
+          <Route path='/settings'>
+              <Settings />
+          </Route>
+      </Router>
     </>
   );
 };
